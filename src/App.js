55b493f3bff96e19ff33a57679c537708daf37a5
Alex <abergsf2017@gmail.com>
2021-09-07 12:33:00 -0700

    test fix blank page

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { Navbar, NavbarBrand, NavItem, Button, Row, Col, Container } from 'react
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Lottie from 'react-lottie';
 import animationData from './Components/the-beautiful-birds-of-heart-valley';
+import video from "./pexels-rostislav-uzunov-5680034.mp4";
 
 const AnyReactComponent = ({ text }) => (
   <div style={{
@@ -44,8 +45,9 @@ function App() {
   return (
     <div>
       <nav>
+        <Container>
         <Navbar className='mynav' expand="md">
-          <Container>
+          
           <NavbarBrand className="navbrand" href="/">Heart of the Valley</NavbarBrand>
           <div className="my-lottie">
             <Lottie 
@@ -54,46 +56,49 @@ function App() {
               width={400}
             />
           </div>
-          <Col xs="7" md="5" lg="5">
+          <Col xs={6} sm={6} md={6} lg={3}>
           <NavItem className='navitems'>
             <Button className="navbutton" href="/">link </Button>
           </NavItem>
-          
-          
           <NavItem className='navitems'>
             <Button className="navbutton" href="/">link </Button>
           </NavItem>
-          
-          
           <NavItem className='navitems'>
             <Button className="navbutton" href="/">link </Button>
           </NavItem>
           </Col>
-          
-          
-          </Container>
-
         </Navbar>
+        </Container>
       </nav>
-    <Container>
       
+
+      <Container>
+      <Row>
+      <div className='my-map' style={{ height: '70vw', width: '100vw' }}>
+      <GoogleMapReact
+        bootstrapURLKeys={{ key: "" }}
+        defaultCenter={defaultProps.center}
+        defaultZoom={defaultProps.zoom}
+      >
+        <AnyReactComponent
+          lat={37.8272}
+          lng={-122.2913}
+          text="My Marker"
+        />
+      </GoogleMapReact>
+    </div>
+    </Row>
+    <Row>
     
-    
-    <div className='my-map' style={{ height: '50vh', width: '30vw' }}>
-    <GoogleMapReact
-      bootstrapURLKeys={{ key: "" }}
-      defaultCenter={defaultProps.center}
-      defaultZoom={defaultProps.zoom}
-    >
-      <AnyReactComponent
-        lat={37.8272}
-        lng={-122.2913}
-        text="My Marker"
-      />
-    </GoogleMapReact>
+    </Row> 
+    </Container>
+    <video width="100vw" opacity="60" autoplay="true" loop muted >
+        <source src={video} type="video/mp4" />
+
+          Sorry, your browser doesn't support embedded videos.
+      </video>  
   </div>
-  </Container>
-  </div>
+  
 );
 }
 
